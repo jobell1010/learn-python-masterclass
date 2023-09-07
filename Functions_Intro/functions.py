fa79b16768b392671c6929c41a9cc24ca9fb2aee
jobell1010 <48512499+jobell1010@users.noreply.github.com>
2023-09-07 13:27:46 +0100

    function learning
    All about docstrings

--- Functions_Intro/functions.py
+++ Functions_Intro/functions.py
@@ -1,28 +1,55 @@
 def multiply(x, y):
+    """
+    Multiply the two given parameters together.
+
+    :param x: First parameter
+    :param y: Second parameter
+    :return: The result of the multiplication
+    """
     result = x * y
     return result
 
 
 def is_palindrome(string):
+    """
+    Return `True` if inputted string is a palindrome.
+
+    :param string: The string to check.
+    :return: `True` if palindrome, `False` if not palindrome
+    """
     # backwards = string[::-1]
     # return backwards == string
     return string[::-1].casefold() == string.casefold()
 
 
 def palindrome_sentence(string):
+    """
+    Return `True` if inputted string is a palindromic sentence,
+    not including non-alphabetic characters, eg. spaces.
+
+    :param string: Sentence to be checked
+    :return: `True` if palindrome, `False` if not palindrome
+    """
     alphanumeric_only = "".join(char for char in string if char.isalpha())
     # return alphanumeric_only[::-1].casefold() == alphanumeric_only.casefold()
     return is_palindrome(alphanumeric_only)
 
 
-# word = input("Please enter a word to check: ")
-# if is_palindrome(word):
-#     print("'{}' is a palindrome".format(word))
-# else:
-#     print("'{}' is not a palindrome".format(word))
+def fibonacci(n):
+    """Return the `n` th Fibonacci number, for positive `n`."""
+    if 0 <= n <= 1:
+        return n
 
-sentence = input("Please enter a sentence to check: ")
-if palindrome_sentence(sentence):
-    print("'{}' is a palindrome".format(sentence))
-else:
-    print("'{}' is not a palindrome".format(sentence))
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
+for i in range(36):
+    print(i, fibonacci(i))
